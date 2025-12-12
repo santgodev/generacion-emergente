11107b8e43f53983aad10ecbc3a346573169d177
Santiago Guevara <santgodev@example.com>
2025-12-12 02:43:46 -0500

    Almost all ready

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WCQs9gyOT9OvcoL/hGReaITvcammmXIcwVaEiiAC3Ro=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9Ku9aWQXqzjWU088iPe8sdTNajzHK0EshxnMwMEA2wI=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'generacion-emergente';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: isProd ? `/${repoName}` : '',
+    assetPrefix: isProd ? `/${repoName}/` : '',
+}
 
 module.exports = nextConfig
